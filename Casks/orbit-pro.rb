b660b53fcf44c0cbb42bf8d551e45ee74659c53d
Developer Artfabric <artfbrc@icloud.com>
2026-09-14 18:55:16 +0300

    orbit-pro 1.1.0

--- Casks/orbit-pro.rb
+++ Casks/orbit-pro.rb
@@ -1,9 +1,9 @@
 # Homebrew cask для Orbit.PRO. Публикуется в tap Coderok-ru/homebrew-orbit-pro (Scripts/publish.sh):
 #   brew install --cask coderok-ru/orbit-pro/orbit-pro
-# Значения 1.0.0/13b062226fef6c8ea4fa8fba2bbf73b1fe346d9773646cc202057685db8c25c0 подставляет Scripts/release.sh.
+# Значения 1.1.0/4b9df8240994f87d6a834e15c07a84a17347e3b8eb0d336e970ac04850f2e85a подставляет Scripts/release.sh.
 cask "orbit-pro" do
-  version "1.0.0"
-  sha256 "13b062226fef6c8ea4fa8fba2bbf73b1fe346d9773646cc202057685db8c25c0"
+  version "1.1.0"
+  sha256 "4b9df8240994f87d6a834e15c07a84a17347e3b8eb0d336e970ac04850f2e85a"
 
   url "https://github.com/Coderok-ru/orbit-pro-site/releases/download/v#{version}/Orbit.PRO-#{version}.dmg"
   name "Orbit.PRO"
